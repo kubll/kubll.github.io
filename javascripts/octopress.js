4493803be8570fa96f308c1b04d05a21d692f052
kubll <577735904@qq.com>
2017-03-25 08:56:41 +0800

    修改日志

--- javascripts/octopress.js
+++ javascripts/octopress.js
@@ -14,14 +14,10 @@ function getNav() {
 
 function addSidebarToggler() {
   if(!$('body').hasClass('sidebar-footer')) {
-    //$('#content').append('<span class="toggle-sidebar"></span>');
+    $('#content').append('<span class="toggle-sidebar"></span>');
     $('.toggle-sidebar').bind('click', function(e) {
       e.preventDefault();
-      if ($('body').hasClass('collapse-sidebar')) {
-        $('body').removeClass('collapse-sidebar');
-      } else {
-        $('body').addClass('collapse-sidebar');
-      }
+      $('body').toggleClass('collapse-sidebar');
     });
   }
   var sections = $('aside.sidebar > section');
